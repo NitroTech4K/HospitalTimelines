eda5f918e8c7479db0d0f33d1ffcee123dad3b20
NitroTech 4K <crtraer@gmail.com>
2022-11-16 10:26:18 -0400

    Almost finished first trigger

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -11,8 +11,10 @@ public class TriggerScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.gameObject.tag == "Player")
         {
+        Debug.Log("Triggered");
             timeline.Play();
 
             if (OneTimeTrigger == true)
